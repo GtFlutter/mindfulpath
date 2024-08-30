abffc9facfaad3717e74ef7d9ce8aeb6d748e305
Divya Sankaliya <gtflutterapps@gmail.com>
2024-08-30 19:28:47 +0530

    #ios folder rollback because by mistake remove and commit wrong files so.

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,25 @@
+import UIKit
+import Firebase
+import Flutter
+import flutter_local_notifications
+
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+      //Notification Setup Start
+    FirebaseApp.configure()
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+                GeneratedPluginRegistrant.register(with: registry)
+            }
+      if #available(iOS 10.0, *) {
+                UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+              }
+      //Notification Setup End
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
